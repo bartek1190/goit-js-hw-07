ef3162fd18ab28cb8e963834cd816150ab1d671e
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-12 09:42:26 +0200

    simply light box

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>
+  `;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join("");
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
